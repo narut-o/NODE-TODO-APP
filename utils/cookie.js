adfb0ac23c13e179f6b8888ddcf39f5477c77fd9
narut-o <akash_ranjan__@outlook.com>
2024-03-20 12:49:02 +0530

    updated jwt duration

--- utils/cookie.js
+++ utils/cookie.js
@@ -4,7 +4,7 @@
 export const cookieOptions = {
     
     httpOnly:true,
-    expires:new Date(Date.now()+60*1000),
+    expires:new Date(Date.now()+15*60*1000),
     sameSite:process.env.NODE_ENV === "PRODUCTION" ? "none" : "lax",
     secure:process.env.NODE_ENV === "PRODUCTION" ? true : false
 };
