4e3f0da8b18d911f36fbc4c0e6f3213b38882f1a
narut-o <akash_ranjan__@outlook.com>
2024-03-20 12:44:56 +0530

    change app mode

--- utils/cookie.js
+++ utils/cookie.js
@@ -5,6 +5,6 @@ export const cookieOptions = {
     
     httpOnly:true,
     expires:new Date(Date.now()+60*1000),
-    sameSite:process.env.NODE_ENV === "DEVELOPMENT" ? "lax" : "none",
-    secure:process.env.NODE_ENV === "DEVELOPMENT" ? false : true
+    sameSite:process.env.NODE_ENV === "PRODUCTION" ? "none" : "lax",
+    secure:process.env.NODE_ENV === "PRODUCTION" ? true : false
 };
