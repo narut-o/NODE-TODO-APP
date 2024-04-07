ab72ca707bffcc6cad92616659157cfe197d4c89
narut-o <akash_ranjan__@outlook.com>
2024-04-07 14:22:16 +0530

    corsConfig updated

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ export const app = express();
 //Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(cors(corsConfig))
 
 //Routes
 app.use("/api/v1/users",userRouter);
